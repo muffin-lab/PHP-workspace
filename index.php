135f1618c032a19ea447fd485a063903dd954227
muffin-lab <kirankannan.v@gmail.com>
2024-07-11 09:15:21 +0530

    commit

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php
 
-require('function.php');
+require 'function.php';
 
-require 'routes.php';
+// require 'routes.php';
+
+
+
+//require Database
+require 'Database.php';
+//class database
+
+$config = require('config.php');
+
+$db = new Database($config['database']);
+
+$id = $_GET['id'];
+
+$query = "SELECT * FROM doctor WHERE id = {$id}";
+
+$posts = $db->query($query)->fetch();
+
+dd($posts);
